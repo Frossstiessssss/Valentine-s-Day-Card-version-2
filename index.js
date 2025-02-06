a8ee941640bbb5c571e566e1abe111047c096807
Frossstiesssssss <rshepcottmanns@gmail.com>
2025-02-06 22:52:04 +0000

    Initialized Valentine Card - working
    - added html
    - added css
    - added java

---
+++ index.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    var envelope = $("#envelope");
+    var btn_open = $("#open");
+    var btn_reset = $("#reset");
+    var btn_no = $("#no");
+    var btn_yes = $("yes");
+
+    envelope.click(function() {
+        open();
+    });
+    btn_open.click(function() {
+        open();
+    });
+    btn_reset.click(function() {
+        close();
+    });
+
+    btn_no.click(function(){
+        newButton();
+    })
+
+    btn_yes.click(function(){
+        nextPage();
+    })
+
+
+    function open() {
+        envelope.addClass("open")
+            .removeClass("close");
+    }
+
+    function close() {
+        envelope.addClass("close")
+            .removeClass("open");
+    }
+
+
+
+    function nextPage(){
+        
+    }
+})
